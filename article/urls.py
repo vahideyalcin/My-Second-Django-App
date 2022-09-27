85e4a8c838e802929f3ba65ecaaa111cfb4e229c
vahideyalcin <vahideyalcin1@gmail.com>
2022-09-27 22:33:10 +0300

    project completed

--- article/urls.py
+++ article/urls.py
@@ -5,10 +5,13 @@ from . import views
 app_name = "article"
 
 urlpatterns = [
-    path('create/',views.index, name ="index"),
+    path('dashboard/',views.dashboard, name ="dashboard"),
 
-    
+    path('addarticle/',views.addArticle, name ="addarticle"),
 
-
-    
+    path('article/<int:id>',views.detail, name ="detail"),
+    path('update/<int:id>',views.updateArticle, name ="update"),
+    path('delete/<int:id>',views.deleteArticle, name ="delete"),
+    path('',views.articles,name ="articles"),
+    path('comment/<int:id>',views.addComment, name ="comment"),
 ]
--- blog/urls.py
+++ blog/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from article import views
+from django.conf import settings
+from django.conf.urls.static import static
 
+from article import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name = "index"),
@@ -26,3 +28,4 @@ urlpatterns = [
 
     
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
